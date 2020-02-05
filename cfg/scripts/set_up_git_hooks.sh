d6323e65a6b1b2686cb44c42ddfbdf24a7f0baed
Francisco José Romero Medina <f.romeromedina@gmail.com>
2020-02-05 23:44:20 +0100

    Arreglar pre-push

--- cfg/scripts/set_up_git_hooks.sh
+++ cfg/scripts/set_up_git_hooks.sh
@@ -2,7 +2,10 @@
 
 PRE_PUSH_FILE='pre-push'
 HOOKS="${PWD}/.git/hooks/"
-if [ ! -f ${HOOKS}${PRE_PUSH_FILE} ]; then
+
+if [ ! -f "${HOOKS}${PRE_PUSH_FILE}" ]; then
   echo "Linkeando pre-push"
-  ln -s ${PRE_PUSH_FILE} ${HOOKS}${PRE_PUSH_FILE}
+  ln -s "${PWD}/${PRE_PUSH_FILE}" "${HOOKS}${PRE_PUSH_FILE}"
+else
+  echo "El archivo ya existe"
 fi
